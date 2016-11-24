0737c9726a94bd8a4d7c3e0a083190105aa29bd9
Devin Chen <d3vin.chen@gmail.com>
2016-11-24 14:28:10 +0800

    update

--- devServer.js
+++ devServer.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 //const multer = require('multer');
 
 const app = express();
-
 app.use(compression());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
