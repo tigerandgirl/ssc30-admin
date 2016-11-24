4805c179f756e911cd1efb3c284a0682924d9765
Devin Chen <d3vin.chen@gmail.com>
2016-11-24 16:22:35 +0800

    add demo server

--- demoServer.js
+++ demoServer.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const compression = require('compression');
 const express = require('express');
-const webpack = require('webpack');
 const bodyParser = require('body-parser');
 //const multer = require('multer');
 
@@ -11,22 +10,8 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 //var upload = multer(); // for parsing multipart/form-data
 
-if (process.env.NODE_ENV === 'production') {
-  var config = require('./webpack.config.prod');
-} else {
-  var config = require('./webpack.config.dev');
-}
-
-const compiler = webpack(config);
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('webpack-hot-middleware')(compiler));
-}
-
 app.use('/', express.static(path.join(__dirname + '/client')));
+app.use('/static', express.static(path.join(__dirname + '/dist')));
 
 app.use(require('./server/routes/fakeApiArch')());
 app.use(require('./server/routes/fakeApiRole')());
@@ -61,5 +46,4 @@ app.listen(port, ip, function (err) {
   }
 
   console.log('Listening at http://%s:%s', ip, port);
-  console.log('webpack is building now, please wait...');
 });
--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,79 +1,58 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    //devtool: 'source-map',
-    //devtool: 'cheap-module-eval-source-map',
-
-    // build files for chrome extension also client
-    entry: {
-      'client/index.build_baozhangren': [
-        './src/index.build_baozhangren'
-      ],
-      'client/index.build_zuoyeren': [
-        './src/index.build_zuoyeren'
-      ]/*,
-      'chrome/index': [
-        './src/index.build_baozhangren'
-      ]*/
-    },
-
-    //输入目标
-    output: {
-      path: path.join(__dirname),
-      filename: '[name].js', //Template based on keys in entry above
-      publicPath: '/client/'
-    },
-
-    //common.js 是公共模块
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('client/common.build.js'),
-        //new webpack.optimize.CommonsChunkPlugin('chrome/common.js'),
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        // // Transfer Files
-        // new TransferWebpackPlugin([
-        //     {from: 'www'},
-        // ], path.resolve(__dirname, 'src')),
-        // new ManifestPlugin({
-        //     fileName:  'manifest.json'
-        // })
-    ],
-
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'stage-0', 'react']
-                },
-                include: __dirname
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.less$/,
-                loader: 'style-loader!css-loader!less-loader'
-            },
-            {
-                test: /\.(png|jpg|bmp)$/,
-                loader: 'url-loader?limit=8192'
-            }
-        ]
-    }
+  entry: {
+    'bundle': [
+      './src/index'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js' //Template based on keys in entry above
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    /**
+     * This plugin assigns the module and chunk ids by occurence count. What this
+     * means is that frequently used IDs will get lower/shorter IDs - so they become
+     * more predictable.
+     */
+    new webpack.optimize.OccurenceOrderPlugin(),
+    /**
+     * See description in 'webpack.config.dev' for more info.
+     */
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        include: __dirname
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.(png|jpg|bmp)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  }
 };
 
 
