b5a18e34df98220d0cdc2fde895956d38435f91a
Devin Chen <d3vin.chen@gmail.com>
2017-02-15 11:03:43 +0800

    deploy to aliyun

---
+++ .utils/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+user=root
+ip=10.3.14.237
+port=22
+
+# Change to source root dir
+utils_dir=`dirname $(readlink -f $0)`
+root_dir=`dirname $utils_dir`
+cd $root_dir
+
+# Building
+#npm run build
+
+# Bundle and upload
+rsync -arvzh -e "ssh -p $port" --progress dist/ $user@$ip:/data/ficloud/uiresources/manaaccount/dist/
