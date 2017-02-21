a28f39661bd8b2c09f23656ac4c6e8cfb6773253
Devin Chen <d3vin.chen@gmail.com>
2017-02-21 12:55:39 +0800

    add style for adin

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
  * Import the stylesheet you want used! Here we just reference
  * the main SCSS file we have in the styles directory.
  */
-import './styles/main.scss';
+//import './styles/main.scss';
+import './styles/ssc-grid.css';
 
 /**
  * Both configureStore and Root are required conditionally.
