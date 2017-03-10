220a9b92965c01b5551b7583730318234d36dd52
Devin Chen <d3vin.chen@gmail.com>
2017-03-10 11:02:30 +0800

    部署到联调服务器

--- .utils/deploy.sh
+++ .utils/deploy.sh
@@ -4,10 +4,16 @@ user=root
 ip=10.3.14.237
 port=22
 
+CLUSTER_NODE237=10.3.14.237
+TESTING_SERVER=172.20.4.88
+
 # Change to source root dir
 utils_dir=`dirname $(readlink -f $0)`
 root_dir=`dirname $utils_dir`
 cd $root_dir
 
 # Bundle and upload
-rsync -arvzh -e "ssh -p $port" --progress dist/ $user@$ip:/data/ficloud/uiresources/manaaccount/
+echo "同步本地编译后的数据到集群中的一个节点 10.3.14.237"
+rsync -arvzh -e "ssh -p $port" --progress --chmod=777 dist/ $user@$CLUSTER_NODE237:/data/ficloud/uiresources/manaaccount/
+echo "同步本地编译后的数据到联调服务器 172.20.4.88"
+rsync -arvzh -e "ssh -p $port" --progress --chmod=777 dist/ $user@$TESTING_SERVER:/ssc/tomcat_dc_integration/webapps/manaaccount/
