265493fb7614f5b696854985a74aa8fd815bdee7
Devin Chen <d3vin.chen@gmail.com>
2017-03-20 14:21:20 +0800

    添加卡demo

--- src/containers/ExternalDataModelling.js
+++ src/containers/ExternalDataModelling.js
@@ -228,115 +228,59 @@ class ExternalDataModelling extends Component {
     return formData;
   }
 
+  handleChange(fieldId, value) {
+    const newState = { ...this.state };
+    newState.formData[fieldId] = value;
+    this.setState(newState);
+  }
+
+  handleSubmit(event, formData) {
+    alert('提交的数据: Form.state.formData: \n' + JSON.stringify(
+      formData,
+      null, '  '));
+    event.preventDefault();
+  }
+
+
   render() {
     const { outerEntityTree } = this.props;
-    console.log(outerEntityTree)
-
-    let treeData = [
-      {
-        'title': '0-0-label',
-        'key': '0-0-key',
-        'children': [
-          {
-            'title': '0-0-0-label',
-            'key': '0-0-0-key',
-            'children': [
-              {
-                'title': '0-0-0-0-label',
-                'key': '0-0-0-0-key'
-              },
-              {
-                'title': '0-0-0-1-label',
-                'key': '0-0-0-1-key'
-              },
-              {
-                'title': '0-0-0-2-label',
-                'key': '0-0-0-2-key'
-              }
-            ]
-          },
-          {
-            'title': '0-0-1-label',
-            'key': '0-0-1-key',
-            'children': [
-              {
-                'title': '0-0-1-0-label',
-                'key': '0-0-1-0-key'
-              },
-              {
-                'title': '0-0-1-1-label',
-                'key': '0-0-1-1-key'
-              },
-              {
-                'title': '0-0-1-2-label',
-                'key': '0-0-1-2-key'
-              }
-            ]
-          },
-          {
-            'title': '0-0-2-label',
-            'key': '0-0-2-key'
-          }
-        ]
-      },
-      {
-        'title': '0-1-label',
-        'key': '0-1-key',
-        'children': [
-          {
-            'title': '0-1-0-label',
-            'key': '0-1-0-key',
-            'children': [
-              {
-                'title': '0-1-0-0-label',
-                'key': '0-1-0-0-key'
-              },
-              {
-                'title': '0-1-0-1-label',
-                'key': '0-1-0-1-key'
-              },
-              {
-                'title': '0-1-0-2-label',
-                'key': '0-1-0-2-key'
-              }
-            ]
-          },
-          {
-            'title': '0-1-1-label',
-            'key': '0-1-1-key',
-            'children': [
-              {
-                'title': '0-1-1-0-label',
-                'key': '0-1-1-0-key'
-              },
-              {
-                'title': '0-1-1-1-label',
-                'key': '0-1-1-1-key'
-              },
-              {
-                'title': '0-1-1-2-label',
-                'key': '0-1-1-2-key'
-              }
-            ]
-          },
-          {
-            'title': '0-1-2-label',
-            'key': '0-1-2-key'
-          }
-        ]
-      },
-      {
-        'title': '0-2-label',
-        'key': '0-2-key'
-      }
-    ];
-
-    treeData = outerEntityTree;
 
     const properties = [
       {label: '显示名称', value: '家庭地址'},
       {label: '外部系统定义的表标签', value: ''}
     ];
+
+const mockFieldsModel = [
+  {type: 'string', id: 'id', label: '主键', hidden: true},
+  {type: 'string', id: 'danjubianhao', label: '单据编号'},
+  {type: 'string', id: 'name2', label: '名称2', hidden: true},
+  {type: 'string', id: 'name3', label: '名称3', hidden: true},
+  {type: 'string', id: 'name4', label: '名称4', hidden: true},
+  {type: 'enum', id: 'danjuleixing', label: '单据类型', placeholder: '请选择单据类型',
+    data: [
+      {key: '2631', value: '差旅费借款单'},
+      {key: '2632', value: '会议费借款单'},
+      {key: 'D3', value: '付款单'}
+    ]
+  },
+  {type: 'double', id: 'jine', label: '金额'},
+  {type: 'date', id: 'danjuriqi', label: '单据日期'},
+  {type: 'boolean', id: 'qiyong', label: '启用'}
+];
+
+const mockFormData = {
+  id: '22EA0EB9-FABA-4224-B290-4D041A1DF773',
+  danjubianhao: 'abc123',
+  name2: '名称2',
+  name3: '名称3',
+  name4: '名称4',
+  danjuleixing: 'D3',
+  jine: '12.00',
+  danjuriqi: new Date('2017-02-14').toISOString(),
+  qiyong: false
+};
+
+
 
     return (
       <div className="external-data-modelling-container">
@@ -344,29 +288,25 @@ class ExternalDataModelling extends Component {
           <Row>
             <Col md={4}>
             {
-              treeData.length === 0
+              outerEntityTree.length === 0
               ? null
               : <SSCTree
                   className="left-tree"
                   showLine
                   checkable
                   defaultExpandAll
-                  treeData={treeData}
+                  treeData={outerEntityTree}
                 />
             }
             </Col>
             <Col md={8}>
               <h3>属性编辑器</h3>
-              <Table bordered>
-                <tbody>
-                  {properties.map((item, index) => (
-                    <tr key={index}>
-                      <td>{item.label}</td>
-                      <td>{item.value}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
+              <Form
+                fieldsModel={mockFieldsModel}
+                defaultData={mockFormData}
+                onChange={::this.handleChange}
+                onSubmit={::this.handleSubmit}
+              />
             </Col>
           </Row>
         </Grid>
