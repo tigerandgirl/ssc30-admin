fbe12377885c73fc08ae363bfa9497b9ce05bcbc
Devin Chen <d3vin.chen@gmail.com>
2017-04-08 16:31:24 +0800

    已经把chrome安装到230上了

--- .jenkins/deploy-yzb.sh
+++ .jenkins/deploy-yzb.sh
@@ -5,17 +5,13 @@ pwd
 set
 echo DEBUG-end
 
-#before_install:
-export CHROME_BIN=chromium-browser
+## before_install:
+#export CHROME_BIN=chromium-browser
 export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+sudo systemctl start xvfb.service
 
-#install:
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update -q
-sudo apt-get install -q google-chrome-stable libfontconfig1
+## install:
 cnpm install
 
-#script:
+## script:
 npm test && npm run release:yzb-local
