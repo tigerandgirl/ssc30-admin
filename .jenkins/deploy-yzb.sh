572ce186e422a99b9e7aeea30532eca2f2be9b89
Devin Chen <d3vin.chen@gmail.com>
2017-04-08 15:37:55 +0800

    部署环境部分扔到jenkins上进行

--- .jenkins/deploy-yzb.sh
+++ .jenkins/deploy-yzb.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/erlang/bin:/home/sscweb/.local/bin:/home/sscweb/bin:/home/sscweb/node-v6.10.0/bin:/home/sscweb/node-v6.10.0/lib/node_modules
 
 echo DEBUG-start
 pwd
 set
 echo DEBUG-end
 
-[ -d ssc30-admin/.git ] || git clone https://github.com/yyssc/ssc30-admin.git
-cd ssc30-admin
-git pull
-
 cnpm install
-npm run release:yzb
+npm test && npm run release:yzb-local
