910ace14554e96563f192f4c1c69f25b5decc7a0
Devin Chen <d3vin.chen@gmail.com>
2017-04-11 20:37:43 +0800

    deploy to 238

--- .jenkins/deploy-yzb.sh
+++ .jenkins/deploy-yzb.sh
@@ -24,15 +24,27 @@ root_dir=`dirname $utils_dir`
 src=dist/
 cd $root_dir
 
-# Bundle and upload
+# 237 也就是 fi.yonyoucloud.com
 echo "同步本地编译后的数据到友账表服务器 10.3.14.237(localhost)"
 cat <<EOT > dist/config.js
 var G_SCHEME = 'https';
 var G_HOST_PORT = 'fi.yonyoucloud.com';
-var G_PATH_PREFIX = /ficloud'';
+var G_PATH_PREFIX = '/ficloud';
 EOT
 rsync -arvzh --delete --progress --chmod=a+rwx $src /data/ficloud/uiresources/manaaccount/
 
 
+# 238
+cat <<EOT > dist/config.js
+var G_SCHEME = 'http';
+var G_HOST_PORT = '10.3.14.238';
+var G_PATH_PREFIX = '/ficloud';
+EOT
+ip=10.3.14.238
+port=22
+user=sscweb
+src=dist/
+dest=/data/ficloud/uiresources/manaaccount
+rsync -arvzh -e "ssh -p $port" --progress --chmod=a+rwx $src $user@$ip:$dest
 
 date
