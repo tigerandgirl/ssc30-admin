8cae45e9a1a9e804dff353dce6ae865e5d8ad959
Devin Chen <d3vin.chen@gmail.com>
2017-04-15 16:54:08 +0800

    add FormPanel component

--- src/containers/MappingDef2CreateForm.js
+++ src/containers/MappingDef2CreateForm.js
@@ -5,13 +5,9 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { browserHistory } from 'react-router';
 import _ from 'lodash';
 
-import { Button, Grid, Row, Col } from 'react-bootstrap';
-import { Form as SSCForm } from 'ssc-grid';
-
-import FormulaField from '../components/FormulaField';
+import FormPanel from '../components/FormPanel';
 import * as Actions from '../actions/mappingDef';
 
 /**
@@ -20,9 +16,10 @@ import * as Actions from '../actions/mappingDef';
  */
 
 class MappingDefCreateForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
 
   componentWillMount() {
     // 如果用户刷新了页面，store中的数据就清空了，需要返回到列表页面再次获取数据
@@ -38,74 +35,24 @@ class MappingDefCreateForm extends Component {
   componentWillReceiveProps(/* nextProps */) {
   }
 
+  handleFormSubmit(formData) {
+    this.props.createTableBodyDataAndFetchTableBodyData(formData);
+  }
+
   render() {
     const {
-      tableColumnsModel,
-      createFormData,
-      createTableBodyDataAndFetchTableBodyData
+      tableColumnsModel
     } = this.props;
 
-    // 准备制作自定义组件 - 公式编辑器
-    const fieldModel = tableColumnsModel.map(({ ...columnModel }) => {
-      if (columnModel.datatype === 20 && columnModel.type === 'custom') {
-        columnModel.component = FormulaField;
-      }
-      return columnModel;
-    });
-
     return (
-      <div
-        className="mapping-def-create-form-container content"
-        style={{
-          padding: '2em'
-        }}
-      >
-        <p className="server-message" style={{ color: 'red' }}>
-          {this.props.serverMessage}
-        </p>
-        <div
-          className="card"
-          style={{
-            padding: '1em',
-            backgroundColor: 'rgb(255, 255, 255)',
-            boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'
-          }}
-        >
-          <Grid
-            style={{
-              padding: 0,
-              margin: 0
-            }}
-          >
-            <Row>
-              <Col md={10}>
-                <h3
-                  style={{
-                    margin: 0
-                  }}
-                >创建转换规则定义</h3>
-              </Col>
-              <Col md={2}>
-                <Button onClick={browserHistory.goBack}>返回列表</Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={12}>
-                <SSCForm
-                  fieldsModel={fieldModel}
-                  defaultData={createFormData}
-                  layout={{
-                    columnCount: 3,
-                    columnWidth: 4
-                  }}
-                  onSubmit={createTableBodyDataAndFetchTableBodyData}
-                  onReset={browserHistory.goBack}
-                />
-              </Col>
-            </Row>
-          </Grid>
-        </div>
-      </div>
+      <FormPanel
+        className="mapping-def-create-form-container"
+        formData={this.props.createFormData}
+        formTitle="创建转换规则定义"
+        onFormSubmit={this.handleFormSubmit}
+        serverMessage={this.props.serverMessage}
+        fieldsModel={tableColumnsModel}
+      />
     );
   }
 }
--- src/containers/MappingDef2EditForm.js
+++ src/containers/MappingDef2EditForm.js
@@ -5,13 +5,9 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { browserHistory } from 'react-router';
 import _ from 'lodash';
 
-import { Button, Grid, Row, Col } from 'react-bootstrap';
-import { Form as SSCForm } from 'ssc-grid';
-
-import FormulaField from '../components/FormulaField';
+import FormPanel from '../components/FormPanel';
 import * as Actions from '../actions/mappingDef';
 
 /**
@@ -20,9 +16,10 @@ import * as Actions from '../actions/mappingDef';
  */
 
 class MappingDefEditForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
 
   componentWillMount() {
     // 如果用户刷新了页面，store中的数据就清空了，需要返回到列表页面再次获取数据
@@ -38,77 +35,30 @@ class MappingDefEditForm extends Component {
   componentWillReceiveProps(/* nextProps */) {
   }
 
+  handleFormSubmit(formData) {
+    this.props.updateTableBodyDataAndFetchTableBodyData(formData);
+  }
+
   render() {
     const {
       tableColumnsModel,
-      tableBodyData,
-      updateTableBodyDataAndFetchTableBodyData
+      tableBodyData
     } = this.props;
 
-    // 准备制作自定义组件 - 公式编辑器
-    const fieldModel = tableColumnsModel.map(({ ...columnModel }) => {
-      if (columnModel.datatype === 20 && columnModel.type === 'custom') {
-        columnModel.component = FormulaField;
-      }
-      return columnModel;
-    });
-
     // 从表格中取出需要编辑的行
-    const formDataObj = tableBodyData.find(row => row.id === this.props.params.id);
+    const formData = tableBodyData.find(
+      row => row.id === this.props.params.id
+    );
 
     return (
-      <div
-        className="mapping-def-edit-form-container content"
-        style={{
-          padding: '2em'
-        }}
-      >
-        <p className="server-message" style={{ color: 'red' }}>
-          {this.props.serverMessage}
-        </p>
-        <div
-          className="card"
-          style={{
-            padding: '1em',
-            backgroundColor: 'rgb(255, 255, 255)',
-            boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'
-          }}
-        >
-          <Grid
-            style={{
-              padding: 0,
-              margin: 0
-            }}
-          >
-            <Row>
-              <Col md={10}>
-                <h3
-                  style={{
-                    margin: 0
-                  }}
-                >编辑转换规则定义</h3>
-              </Col>
-              <Col md={2}>
-                <Button onClick={browserHistory.goBack}>返回列表</Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={12}>
-                <SSCForm
-                  fieldsModel={fieldModel}
-                  defaultData={formDataObj}
-                  layout={{
-                    columnCount: 3,
-                    columnWidth: 4
-                  }}
-                  onSubmit={updateTableBodyDataAndFetchTableBodyData}
-                  onReset={browserHistory.goBack}
-                />
-              </Col>
-            </Row>
-          </Grid>
-        </div>
-      </div>
+      <FormPanel
+        className="mapping-def-edit-form-container"
+        formData={formData}
+        formTitle="编辑转换规则定义"
+        onFormSubmit={this.handleFormSubmit}
+        serverMessage={this.props.serverMessage}
+        fieldsModel={tableColumnsModel}
+      />
     );
   }
 }
