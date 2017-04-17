a45e369ae3eff03a38701539a5c0930b7a661bc8
Devin Chen <d3vin.chen@gmail.com>
2017-04-17 14:10:14 +0800

    add functions script for deployment

--- .jenkins/deploy-ybz.sh
+++ .jenkins/deploy-ybz.sh
@@ -2,34 +2,13 @@
 
 # 友报账
 
+scripts_dir="$(dirname "$0")"
+. "$scripts/functions.sh"
+
 echo DEBUG-start
 pwd
 set
 echo DEBUG-end
-
-## functions
-
-# gen config.js
-gen_config() {
-  scheme="$1"
-  host_port="$2"
-  path_prefix="$3"
-  cat <<EOT > dist/config.js
-var G_SCHEME = '$scheme';
-var G_HOST_PORT = '$host_port';
-var G_PATH_PREFIX = '$path_prefix';
-EOT
-}
-
-# sync files to remote
-sync_files() {
-  ssh_ip="$1"
-  ssh_port="$2"
-  ssh_user="$3"
-  rsync_src="$4"
-  rsync_dest="$5"
-  rsync -arvzh -e "ssh -p $ssh_port" --progress --chmod=a+rwx $rsync_src $ssh_user@$ssh_ip:$rsync_dest
-}
 
 ## before_install:
 export CHROME_BIN=google-chrome
@@ -41,12 +20,6 @@ cnpm install
 ## script:
 #npm test
 npm run build:ybz
-
-# Change to source root dir
-utils_dir=`dirname $(readlink -f $0)`
-root_dir=`dirname $utils_dir`
-src=dist/
-cd $root_dir
 
 # 官网
 gen_config 'https' 'ybz.yonyoucloud.com' ''
