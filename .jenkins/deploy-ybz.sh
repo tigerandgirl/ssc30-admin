d090e8350fb1849d2b8983ce56fe12a3671f6c53
qiufh <qiufh@yonyou.com>
2017-05-22 11:18:34 +0800

    为了友报账的自己开发的基础档案API而修改，为啥资源不共享呢，共享服务听精神何在！

--- .jenkins/deploy-ybz.sh
+++ .jenkins/deploy-ybz.sh
@@ -24,21 +24,21 @@ npm run build:ybz
 # 官网
 gen_config 'https' 'ybz.yonyoucloud.com' ''
 sync_files '10.3.14.233' '22' 'sscweb' 'dist/' \
-  '/server/tomcat_ssc/webapps/manaaccount'
+  '/server/tomcat_ssc/webapps/manaaccountybz'
 
 # 5088 以后的测试环境
 gen_config 'http' '172.20.4.88:5088' ''
 sync_files '172.20.4.88' '22' 'root' 'dist/' \
-  '/ssc/tomcat_dc_integration_2/webapps/manaaccount/'
+  '/ssc/tomcat_dc_integration_2/webapps/manaaccountybz/'
 
 # 6088
 gen_config 'http' '172.20.4.88:6088' ''
 sync_files '172.20.4.88' '22' 'root' 'dist/' \
-  '/ssc/tomcat_dc_integration_3/tomcat_dc_integration/webapps/manaaccount'
+  '/ssc/tomcat_dc_integration_3/tomcat_dc_integration/webapps/manaaccountybz'
 
 # 234 鬼知道这是啥环境
 gen_config 'http' '10.3.14.234' ''
 sync_files '10.3.14.234' '22' 'sscweb' 'dist/' \
-  '/server/tomcat_integration/webapps/manaaccount'
+  '/server/tomcat_integration/webapps/manaaccountybz'
 
 date
