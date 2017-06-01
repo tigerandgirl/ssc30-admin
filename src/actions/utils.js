4b8bcc59d7499efebc16931f9dbc3625dd261c7f
chenli <chenliw@yonyou.com>
2017-06-01 19:18:51 +0800

    职务

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -254,6 +254,10 @@ export function setRequiredFields(baseDocId, { ...field }) {
       sex: true,
       email: true,
       positionstate: true
+    },
+    duty:{
+      duty_code:true ,
+      duty_name:true
     }
   };
   if (data[baseDocId] && data[baseDocId][field.id] === true) {
